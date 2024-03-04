996e9e2a546793118912d7b66482e0e77e1cf9a3
Stefany Cantero <stefanycanteroc@gmail.com>
2024-03-04 09:49:42 -0500

    Country scientific production world map

--- country_scientific_prodcution.py
+++ country_scientific_prodcution.py
@@ -1,1 +1,91 @@
 """Taller Presencial Evaluable"""
+
+import pandas as pd
+import folium as fo
+
+def load_affiliations():
+    dataframe = pd.read_csv(
+        "https://raw.githubusercontent.com/jdvelasq/datalabs/master/datasets/scopus-papers.csv",
+        sep=",", index_col=None
+    )[['Affiliations']]
+    return dataframe
+
+
+def remove_na_rows(affiliations):
+    affiliations = affiliations.copy()
+    affiliations = affiliations.dropna(subset=['Affiliations'])
+    return affiliations
+
+
+def add_countries_column(affiliations):
+    """Transforma la columna 'Affiliations' a una lista de paises."""
+
+    affiliations = affiliations.copy()
+    affiliations["countries"] = affiliations["Affiliations"].copy()
+    # Separar las afiliaciones por punto y coma
+    affiliations["countries"] = affiliations["countries"].str.split(";")
+    # Separa cada afiliación por coma creando una lista de listas
+    affiliations["countries"] = affiliations["countries"].map(
+        lambda x: [y.split(",") for y in x]
+    )
+    # Obtener el pais de cada afiliación (último elemento de la lista)
+    affiliations["countries"] = affiliations["countries"].map(
+        lambda x: [y[-1].strip() for y in x]
+    )
+    # Eliminar los paises repetidos de cada lista
+    affiliations["countries"] = affiliations["countries"].map(set)
+    # Unir los paises en una cadena separada por coma
+    affiliations["countries"] = affiliations["countries"].str.join(", ")
+
+    return affiliations
+
+
+def clean_countries(affiliations):
+    affiliations = affiliations.copy()
+    affiliations["countries"] = affiliations["countries"].str.replace("United States", "United States of America")
+
+    return affiliations
+
+
+def count_country_frequency(affiliations):
+    """Cuenta la cantidad de afiliaciones por país."""
+
+    affiliations = affiliations.copy()
+    countries = affiliations["countries"].str.split(", ")
+    countries = countries.explode()
+    countries = countries.value_counts()
+    return countries
+
+def plot_world_map(countries):
+    """Grafica un mapa mundial con la frecuencia de cada país."""
+
+    countries = countries.copy()
+    countries = countries.to_frame()
+    countries = countries.reset_index()
+
+    m = fo.Map(location=[0, 0], zoom_start=2)
+
+    fo.Choropleth(
+        geo_data="https://raw.githubusercontent.com/python-visualization/folium/master/examples/data/world-countries.json",
+        data=countries,
+        columns=["countries", "count"],
+        key_on="feature.properties.name",
+        fill_color="Greens",
+    ).add_to(m)
+
+    m.save("map.html")
+
+
+def main():
+    """Función principal"""
+    affiliations = load_affiliations()
+    affiliations = remove_na_rows(affiliations)
+    affiliations = add_countries_column(affiliations)
+    affiliations = clean_countries(affiliations)
+    countries = count_country_frequency(affiliations)
+    countries.to_csv("countries.csv")
+    plot_world_map(countries)
+
+
+if __name__ == "__main__":
+    main()
